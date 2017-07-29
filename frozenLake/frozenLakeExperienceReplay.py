f226b1300a8375611b7d99b94c40cf0b1a53f405
streklin <gene.foxwell@gmail.com>
2017-07-29 12:23:21 -0400

    bound outputs with tanh, reduce initial learning rate

--- frozenLake/frozenLakeExperienceReplay.py
+++ frozenLake/frozenLakeExperienceReplay.py
@@ -32,14 +32,14 @@ class ExperienceReplay:
 
 class SingleLayerNetwork:
     def __init__(self):
-        self.initial_learning_rate = 0.1
+        self.initial_learning_rate = 0.01
         self.regularization_param = 0.001
 
     def configure_network(self):
         #network structure
         self.state = tf.placeholder(shape=[1, 16], dtype=tf.float32)
         self.weights = tf.Variable(tf.random_uniform([16, 4], 0, 0.01))
-        self.output = tf.matmul(self.state, self.weights)
+        self.output = tf.tanh(tf.matmul(self.state, self.weights))
         self.prediction = tf.argmax(self.output, 1)
 
         #training parameters
@@ -64,7 +64,7 @@ class SingleLayerNetwork:
 
 #general constants
 number_of_random_episodes = 10000
-number_of_episodes = 20
+number_of_episodes = 10
 moves_per_episode = 99
 epsilon = 0.1
 gamma = 0.98
@@ -72,7 +72,7 @@ sample_size = 2000
 eval_steps = 100
 
 #object for taking the most recent events
-experienceReplay = ExperienceReplay(memory_size=100000)
+experienceReplay = ExperienceReplay(memory_size=10000)
 network = SingleLayerNetwork()
 network.configure_network()
 
@@ -103,8 +103,7 @@ def shape_reward(reward, done):
         return -1
 
     #otherwise, return whatever the reward was (probably zero)
-    #return -0.001 #living penalty
-    return reward
+    return -0.001 #living penalty
 
 def run_random_episodes():
     for i in range(number_of_random_episodes):
@@ -139,11 +138,15 @@ def train_network(session):
     network.reset_network(session)
 
     for i in range(number_of_episodes):
+
+        #keep track of how well the network is doing
+        performance = evaluate_performance(session, False)
+
         s = env.reset()
         state = transform_state_to_tensor(s)
 
         #track its output to keep me from getting paranoid
-        print "Episode: ", i
+        print "Episode: ", i, " current performance: ", performance
 
         for m in range(moves_per_episode):
             #predict the next action
@@ -165,33 +168,36 @@ def train_network(session):
             #add this move to our collection of experiences
             experienceReplay.addEvent(next_state, state, reward, action[0], done)
 
-            #get a training sample
-            batch = experienceReplay.getSamples(sample_size=sample_size)
-
-            #update weights with the sample data
-            for sample in batch:
-                #get the Q values for the start state
-                action, qValues = network.predict_action(sample['prev_state'], session)
-
-                #set the action tensor to the sample action
-                action[0] = sample['action']
-
-                #get the best action for the target state
-                _, targetQ = network.predict_action(sample['state'], session)
-                maxQ = np.max(targetQ)
-
-                #perform a bellmen update on the Q values
-                qValues = bellmen_update(reward=sample['reward'], gamma=gamma, maxQ=maxQ, q_values=qValues, action=action, done=sample['done'])
-
-                #call the train function
-                network.update_network(sample['prev_state'], qValues, session)
-
-            #if done, don't bother with more turns
+            # if done, don't bother with more turns
             if done:
                 break
 
+        #get a training sample
+        batch = experienceReplay.getSamples(sample_size=sample_size)
 
-def evaluate_performance(session):
+        #update weights with the sample data
+        for sample in batch:
+            #get the Q values for the start state
+            action, qValues = network.predict_action(sample['prev_state'], session)
+
+            #set the action tensor to the sample action
+            action[0] = sample['action']
+
+            #get the best action for the target state
+            _, targetQ = network.predict_action(sample['state'], session)
+            maxQ = np.max(targetQ)
+
+            #perform a bellmen update on the Q values
+            qValues = bellmen_update(reward=sample['reward'], gamma=gamma, maxQ=maxQ, q_values=qValues, action=action, done=sample['done'])
+
+            #call the train function
+            network.update_network(sample['prev_state'], qValues, session)
+
+
+#run the agent without any random actions to see how well its learned the optimal policy
+#due to stochasticity in the environment, we can never win 100%, but a result of around
+#80% or so should be considered reasonable.
+def evaluate_performance(session, render_environment):
     rewardList = []
 
     for i in range(eval_steps):
@@ -209,8 +215,9 @@ def evaluate_performance(session):
             next_state = transform_state_to_tensor(s1)
             state = next_state
 
-            #might as well watch the show
-            env.render()
+            #render as needed
+            if render_environment:
+                env.render()
 
             rewardList.append(rSum)
 
@@ -218,15 +225,14 @@ def evaluate_performance(session):
 
 def experience_replay_alg():
     # prime the experience replay with some random experiences
-    experienceReplay.clear()
+    #experienceReplay.clear()
 
     print "Generating Experience Replay Data"
     run_random_episodes()
 
     with tf.Session() as session:
         train_network(session)
-        evaluate_performance(session)
-        performance = evaluate_performance(session)
+        performance = evaluate_performance(session, True)
         print "Percent of sucessful episodes: " + str(performance) + "%"
         return performance
 
@@ -235,8 +241,7 @@ env = gym.make('FrozenLake-v0')
 
 #performance of the network is a bit unstable, running multiple times
 #and averaging the result to get a good measure of the performance
-
-num_iterations = 10
+num_iterations = 1
 results = []
 
 for i in range(num_iterations):
